198b9a959b58bd0432a1dda4d1c8399e5e6c5707
niuyutao <50771267+niuyutao@users.noreply.github.com>
2019-08-30 09:57:47 +0800

    Create docker-onekey-deploy.sh

---
+++ docker-onekey-deploy.sh
@@ -0,0 +1,22 @@
+#安装wget
+yum -y install wget 
+#备份官方yum源
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+#下载阿里云yum源
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+#下载Docker源
+wget -O /etc/yum.repos.d/ https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+#生成yum缓存
+yum makecache
+#安装docker
+yum -y install docker-ce
+#使用daocloud加速器
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://ef017c13.m.daocloud.io
+#启动docker
+systemctl start docker
+#设置docker开机启动
+systemctl enable docker
+
+#配置docker插件
+ wget https://github.com/niuyutao/docker/blob/master/.bashrc_docker
+ echo "[ -f ~/.bashrc_docker ] && . ~/.bashrc_docker" >> ~/.bashrc; source ~/.bashrc
